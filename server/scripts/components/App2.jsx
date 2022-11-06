8810816869b364df2fba70287852582d42bc9ce8
crfmc <crf85@cornell.edu>
2022-11-06 10:33:23 -0500

    test state variables

--- server/scripts/components/App2.jsx
+++ server/scripts/components/App2.jsx
@@ -3,9 +3,10 @@ import { h, Component } from 'preact';
 export default class App2 extends Component {
   constructor() {
     super();
+    this.name = "APP2";
   }
 
   render() {
-    return <button>SERVER HELLO</button>;
+    return <button onClick={() => {console.log('App2 clicked')}}>{this.name}</button>;
   }
 }
